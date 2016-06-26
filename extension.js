40526143ed835edc3893795aaa372fbf4a4c27af
supersaftsack349 <chris@village-town.de>
2016-06-26 22:43:55 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 (function () {
 
     // Change this to your GitHub username so you don't have to modify so many things.
-    var fork = "basicBot";
+    var fork = "supersaftsack349";
 
     // Define our function responsible for extending the bot.
     function extend() {

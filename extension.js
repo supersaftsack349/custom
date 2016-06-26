39e62ff3cf8326016ccab7f08ffafa98e55e8a57
supersaftsack349 <chris@village-town.de>
2016-06-26 22:45:00 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,8 +48,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "basicBot",
       language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/supersaftsack349/source/master/lang/en.json",
+      scriptLink: "https://rawgit.com/supersaftsack349/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
@@ -105,9 +105,9 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/supersaftsack349/custom/master/blacklists/NSFWlist.json",
+        OP: "https://rawgit.com/supersaftsack349/custom/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/supersaftsack349/custom/master/blacklists/BANNEDlist.json"
       }
     }));
 

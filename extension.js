47727d530104f7b4b70637614f58208b4864bbaf
supersaftsack349 <chris@village-town.de>
2016-06-28 22:23:40 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@
         //Extend the bot here, either by calling another function or here directly.
 
         // You can add more spam words to the bot.
-        var spamWords = ['spam1', 'spam2', 'spam3', 'spam4'];
+        var spamWords = 'spam', 'nigger', 'nigger', 'cunt';
         for (var i = 0; i < spamWords.length; i++) {
           window.bot.chatUtilities.spam.push(spamWords[i]);
         }
